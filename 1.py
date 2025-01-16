e71680b7c3227fb13cff278c7063776000826d31
Artem Saifullin <467332@niuitmo.ru>
2025-01-16 22:15:17 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,30 +1,19 @@
-mass=[]
+mass = []
 def is_palindrome(n):
     str_n = str(n)
-    left = 0
-    right = len(str_n) - 1
-    cnt = 0
-    while left < right:
-        if str_n[left] != str_n[right]:
-            cnt += 1
-            if cnt > 1:
-                return False
-        left += 1
-        right -= 1
-    return True
+    return str_n == str_n[::-1]
 for n in range(1, 1000):
-    n2=str(bin(n)[2:])
-    if is_palindrome(int(n2, 2)) == True:
-        n2 =  '10' + n2 + '1'
-    if is_palindrome(int(n2, 2))==False:
-        n2+=n2[::-1]
-        n2='10' + n2 + '1'
-    r=int(n2, 2)
-    print(r)
-    if (is_palindrome(r)==True):
+    n2 = str(bin(n)[2:])
+    if is_palindrome(int(n2, 2)):
+        n2 = '1' + n2 + '1'
+    else:
+        n2 += n2[::-1]
+        n2 = '1' + n2 + '1'
+    r = int(n2, 2)
+    if is_palindrome(r):
         mass.append(r)
-mass.sort(reverse=True)
+mass.sort()
 for i in range(len(mass)):
-    if mass[i]<1000 and str(mass[i])==str(mass[i])[::-1]:
+    if mass[i] > 1000:
         print(mass[i])
         break
