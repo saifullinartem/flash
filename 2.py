0c54818fd158f64773f4d1a1bd1edd49ef4dec80
Artem Saifullin <467332@niuitmo.ru>
2025-01-16 22:18:47 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,29 @@
+mass = []
+def is_palindrome(n):
+    str_n = str(n)
+    left = 0
+    right = len(str_n) - 1
+    cnt = 0
+    while left < right:
+        if str_n[left] != str_n[right]:
+            cnt += 1
+            if cnt > 1:
+                return False
+        left += 1
+        right -= 1
+    return True
+for n in range(1, 1000):
+    n2 = str(bin(n)[2:])
+    if is_palindrome(int(n2, 2)):
+        n2 = '10' + n2 + '1'
+    if not is_palindrome(int(n2, 2)):
+        n2 += n2[::-1]
+        n2 = '10' + n2 + '1'
+    r = int(n2, 2)
+    if is_palindrome(r):
+        mass.append(r)
+mass.sort(reverse=True)
+for i in range(len(mass)):
+    if mass[i] < 1000 and str(mass[i]) == str(mass[i])[::-1]:
+        print(mass[i])
+        break
